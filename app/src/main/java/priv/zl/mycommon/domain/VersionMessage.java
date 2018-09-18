d2898534eda3c4e9cdfe5f04e75ca4301ecd1d6f
z588328800 <116890939@qq.com>
2018-09-18 10:10:21 +0800

    新闻客户端主页完成---添加版本检测更新功能

---
+++ app/src/main/java/priv/zl/mycommon/domain/VersionMessage.java
@@ -0,0 +1,14 @@
+package priv.zl.mycommon.domain;
+
+/**
+ * 版本信息
+ */
+public class VersionMessage {
+    public String downloadUrl;
+    public int versionCode;
+    public String versionDes;
+    public String versionName;
+
+    public VersionMessage() {
+    }
+}
